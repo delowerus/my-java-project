36f38fef0db63a5c0a343ede3b9d6f4486c0c6c4
Delower Hossen <delowerus@gmail.com>
2020-09-10 03:53:01 -0400

    Swapping array

---
+++ src/arrays/SwappingArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class SwappingArray {
+    public static void main(String[] args) {
+
+        //how to swap value
+
+        int a = 6;
+        int b = 7;
+
+        int temp;//creating new variable
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("the value of a is " + a);
+        System.out.println("the value of b is " + +b);
+    }
+}
