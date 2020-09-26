7baff2a21bddf38313313fd36c7bb165979de97e
Delower Hossen <delowerus@gmail.com>
2020-09-26 01:55:10 -0400

    LinearSearch.java

---
+++ src/arrays/LinearSearch.java
@@ -0,0 +1,26 @@
+package arrays;
+
+/* Homework
+* int[] arr = {1, 2, 3, 4, 5, 6}
+* int x;
+*
+* x = 1; index = 0
+*
+* x = 4, index = 3
+*
+* x = 10, index = -1
+* */
+
+public class LinearSearch {
+
+    public boolean search(int[] arr, int x){
+        // for loop
+        // equal condition, if arr[i] == x
+        // return statement
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
