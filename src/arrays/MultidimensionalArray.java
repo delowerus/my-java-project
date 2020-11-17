6c17ebabd1389236abb590878478508f28831fd9
Delower Hossen <delowerus@gmail.com>
2020-11-17 17:36:32 -0500

    modified

--- src/arrays/MultidimensionalArray.java
+++ src/arrays/MultidimensionalArray.java
@@ -1,38 +1,66 @@
 package arrays;
 
+/*
+* 4 * 4 , m * n, rowIndex = 0..... m - 1, colIndex = 0.....n - 1
+*  i = 0...3, j = 0...3
+*   0,0       0,1       0,2       0,3
+*
+*   1,0       1,1       1,2       1,3
+*
+*   2,0       2, 1      2,2       2,3
+*
+*   3,0       3,1       3,2       3,3
+*
+*   1   2   3   4
+*
+*   5   6   7   8
+*
+*   9   10  11  12
+*
+*   13  14  15  16
+* */
+
 public class MultidimensionalArray {
     public static void main(String[] args) {
-        int arr[][] = new int[2][3];//Declaring multidimensional array and allocating memory
+        int row = 6, col = 6;
+        int arr[][] = new int[row][col];//Declaring two dimensional array and allocating memory
         /**
          * Here two rows three column
          * ist row..2,5,4
          * 2nd row..6,9,7
          */
-        arr[0][0] = 2;
-        arr[0][1] = 5;
-        arr[0][2] = 4;
+        // print row and col indices
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(i + "," + j + "    ");
+            }
+            System.out.println();
+        }
 
-        arr[1][0] = 6;
-        arr[1][1] = 9;
-        arr[1][2] = 7;
-       // System.out.println(arr[1][0]);
-        //System.out.println(arr[0][2]);
+        // print empty array
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println();
+        }
 
-        //initializing multidimensional array
-        int[][]arr1= {{2,5,4},{6,9,7}};
-
-        //using for loop to traverse through the row
-        //and 2nd loop traverse through the column
-        for(int i = 0; i < 2; i++){
-            for (int j = 0; j < 3; j++){
-                System.out.println(arr[i][j]);
-
+        // insert values or initialize the array
+        int k = 1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // a[i] = k, arr[i][j] = k
+                arr[i][j] = k;
+                k++;
             }
         }
 
-
-
-
-
+        // print the array after inserting values
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print(arr[i][j] + "    ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/arrays/PrintArray.java
+++ src/arrays/PrintArray.java
@@ -4,9 +4,12 @@ public class PrintArray {
     public static void main(String[] args) {
         //Create an array of type int
         int[] numbers = new int[5];
+
         //insert values
+        int k = 1;
         for( int i = 0; i < numbers.length; i++){
-            numbers[i]= i + 1;
+            numbers[i]=  k;
+            k++;
         }
         //print array
         for(int i = 0; i < numbers.length; i++){
